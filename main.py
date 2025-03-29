e4176c892afcadbe0d43ac30fc37385c87fe5339
DevER-M <sankam.mithunganeshviib@gmail.com>
2025-03-29 15:23:24 +0530

    better

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from collections.abc import Callable, Buffer
 from _hashlib import HASH
+from functools import partial
+from collections.abc import Callable, Buffer
 
 
-def chunks(filepath: str, chunksize: int, hasher: Callable[[Buffer], HASH]):
-    with open(filepath, "rb") as f:
-        while f.read(1) != b"":
-            print(hasher(f.read(chunksize)).hexdigest())
+def chunk_hasher(filepath: str, chunksize: int, hasher: Callable[[Buffer], HASH]):
+    with open(filepath, 'rb') as f:
+        # partial creates function with default arg chunksize
+        for chunk in iter(partial(f.read, chunksize), b''):
+            # iter calls f.read(chunksize) until it returns b'' i.e the sentinel
+            yield hasher(chunk)
 
 
 def tests(filepath):
     from hashlib import sha1
-    chunks(filepath, 1024,sha1)
+
+    print(chunk_hasher(filepath, 1024, sha1))
 
 
-if __name__ == "__main__":
-    tests("randombin")
+if __name__ == '__main__':
+    tests('randombin')
